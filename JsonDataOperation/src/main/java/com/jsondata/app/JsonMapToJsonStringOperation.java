f7bd48eb9d734cf8cbf1e1792757be2d8d500834
snortintelligence <snortintelligence@gmail.com>
2022-05-26 00:57:36 +0530

    Convert Map into jsonString. how json works with java

---
+++ JsonDataOperation/src/main/java/com/jsondata/app/JsonMapToJsonStringOperation.java
@@ -0,0 +1,31 @@
+package com.jsondata.app;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonMapToJsonStringOperation {
+	public static void main(String[] args) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			LinkedHashMap<String, Object> mapped = new LinkedHashMap<String, Object>();
+
+			mapped = objectMapper.readValue(JsonString.getJsonString(),
+					new TypeReference<LinkedHashMap<String, Object>>() {
+					});
+
+			System.out.println(mapped);
+			
+			String jsonString = objectMapper.writeValueAsString(mapped);
+			
+			System.out.println("jsonString \n"+jsonString);
+			
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
